690c4140b63ce2a9b24b69c1029cf74c1337474d
Yoann Poupart <yoann.poupart@ens-lyon.org>
2024-09-16 16:52:31 +0200

    new project archi

---
+++ src/vec_noise/__init__.py
@@ -0,0 +1,21 @@
+"""Noise functions for procedural generation of content
+
+Contains native code implementations of Perlin improved noise (with
+fBm capabilities) and Perlin simplex noise. Also contains a fast
+"fake noise" implementation in GLSL for execution in shaders.
+
+Copyright (c) 2008, Casey Duncan (casey dot duncan at gmail dot com)
+Copyright (c) 2017, Zev Benjamin <zev@strangersgate.com>
+Copyright (c) 2024, Yoann Poupart <yoann.poupart@ens-lyon.org>
+"""
+
+__version__ = "2.0.0"
+
+from . import _perlin, _simplex
+
+snoise2 = _simplex.noise2
+snoise3 = _simplex.noise3
+snoise4 = _simplex.noise4
+pnoise1 = _perlin.noise1
+pnoise2 = _perlin.noise2
+pnoise3 = _perlin.noise3
