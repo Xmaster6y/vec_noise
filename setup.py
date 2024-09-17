e1961079e1a51951aa118719c52bd6704c0c981b
Yoann Poupart <yoann.poupart@ens-lyon.org>
2024-09-17 10:12:17 +0200

    new build config

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 import sys
 
 from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext as _build_ext
 
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
+import numpy
 
 if sys.platform != 'win32':
     compile_args = ['-funroll-loops']
@@ -60,13 +53,14 @@ for convenient generation of turbulent noise are also included.
     packages=['vec_noise'],
     setup_requires=['numpy'],
     install_requires=['numpy'],
-    cmdclass={'build_ext': build_ext},
     ext_modules=[
         Extension('vec_noise._simplex', ['_simplex.c'],
             extra_compile_args=compile_args,
+            include_dirs=[numpy.get_include()],
         ),
         Extension('vec_noise._perlin', ['_perlin.c'],
             extra_compile_args=compile_args,
+            include_dirs=[numpy.get_include()],
         )
     ],
 )
